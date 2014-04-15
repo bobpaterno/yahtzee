c098b3412ff2857def888ea7776581b884127d60
Robert Paterno <bobpaterno@gmail.com>
2014-04-15 15:49:22 -0500

    added arrow and row movement and control

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Yahtzee
 
 This is a multi-player Yahtzee game.
+
+Features:
+* Manual scoring, you get to do the math!
--- app/js/source/main.js
+++ app/js/source/main.js
@@ -3,12 +3,109 @@
 
   $(document).ready(init);
 
+  var currUser = 0;
+  var currRoll = 2;
+
   function init() {
     $('#add').click(add);
+    $('.arrow').click(arrow);
+    $('body').keydown(move);
+    $('#add-score').click(score);
   }
 
-  function add() {
-    
+  function score(event) {
+    var newscore = $('#score').val();
+    newscore++;
+    debugger;
+
+    event.preventDefault();
+  }
+
+  function move(event) {
+    var keyCode = event.keyCode;
+    switch(keyCode) {
+    case 38:
+      currUser--;
+      break;
+    case 40:
+      currUser++;
+      break;
+    case 37:
+      currRoll--;
+      break;
+    case 39:
+      currRoll++;
+    }
+    paintScreen();
+
+    if(keyCode<=40 && keyCode>=37) {
+      event.preventDefault();
+    }
+  }
+
+  function arrow() {
+    switch(this.id) {
+      case 'up':
+        currUser--;
+        break;
+      case 'down':
+        currUser++;
+        break;
+      case 'left':
+        currRoll--;
+        break;
+      case 'right':
+        currRoll++;
+    }
+    paintScreen();
+  }
+
+  function paintScreen() {
+    var $trs = $('#game > tbody > tr');
+    var tr = $trs[currUser];
+    $('.horizontal').removeClass();
+    $(tr).addClass('horizontal');
+
+    var $tds = $('#game > tbody > tr > td:nth-child('+currRoll+')');
+    $('.vertical').removeClass();
+    $tds.addClass('vertical');
+  }
+
+  function add(event) {
+      var username = $('#username').val();
+      var avatar = $('#avatar').val();
+
+      createRow(username, avatar);
+
+      event.preventDefault();
+  }
+
+  function createRow(name, avatarurl) {
+    var $tr = $('<tr>');
+    var tds = [];
+    var count;
+
+    for(var i=0; i<16; i++) {
+      tds.push('<td></td>');
+    }
+    $tr.append(tds);
+
+    $('#game > tbody').append($tr);
+
+    count = $('#game > tbody > tr').length;
+    if(count === 1) {
+      $tr.addClass('horizontal');
+    }
+
+    var $img = $('<img>');
+    $img.attr('src', avatarurl);
+    $img.addClass('avatar');
+
+    $tr.children('td:nth-child(1)').append($img);
+    $tr.children('td:nth-child(2)').text(name);
+    $tr.children('td:nth-child(3)').addClass('vertical');
+
+
   }
 
 }());
